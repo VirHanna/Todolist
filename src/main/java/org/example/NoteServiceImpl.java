b5036890aaef5f2f6c2b25377db25ba896a0196e
VirHanna <annabasha527@gmail.com>
2025-04-24 20:25:30 +0300

    first commit

---
+++ src/main/java/org/example/NoteServiceImpl.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class NoteServiceImpl implements NoteService {
+    private final NoteRepository noteRepository;
+
+    public NoteServiceImpl(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    @Override
+    public List<Note> listAll() {
+        return noteRepository.findAll();
+    }
+
+    @Override
+    public Note add(Note note) {
+        return noteRepository.save(note);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        if (!noteRepository.existsById(id)) {
+            throw new NoSuchElementException("Note not found");
+        }
+        noteRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(Note note) {
+        Note existingNote = noteRepository.findById(note.getId())
+                .orElseThrow(() -> new NoSuchElementException("Note not found"));
+
+        existingNote.setTitle(note.getTitle());
+        existingNote.setContent(note.getContent());
+
+        noteRepository.save(existingNote);
+    }
+
+    @Override
+    public Note getById(long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Note not found"));
+    }
+}
